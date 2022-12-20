73ed5c564dc0679265db4ffd0593ff7930a8d3cc
choj96 <choj9603@gmail.com>
2022-12-20 18:31:21 +0900

    정리

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,17 +1,3 @@
-// import React from "react";
-
-// const Detail = () => {
-//   <div className="detail-box">
-//     <div>
-//       <div className="header-bt">
-//         <h2 className="header-txt"></h2>
-//       </div>
-//     </div>
-//   </div>;
-
-// };
-
-// export default Detail;
 import React from "react";
 
 const Detail = (props) => {
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,10 +2,6 @@ import React from "react";
 import { useLocation } from "react-router";
 
 const NotFound = () => {
-  // useLocation Hook 은 location 리턴
-  // pathname: 현재 요청된 경로
-  // search: 쿼리문자열
-  // state : navigate()로 이동할 때 전달된 state
   const location = useLocation();
   return (
     <div className="m-3">
