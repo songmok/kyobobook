c82f5dbb9185682ca08df5d103a730f1a9bfe3eb
choj96 <choj9603@gmail.com>
2022-12-20 11:06:18 +0900

    기초파일 정리

--- src/pages/Cook.js
+++ src/pages/Cook.js
@@ -1,16 +1,6 @@
 import React from "react";
 
-const Cook = (props) => {
-  const list = props.cook.map((item, index) => {
-    return (
-      <div>
-        <p>{item.biTitle}</p>
-        <p>{item.biPrice}</p>
-        <p>{item.biPublisher}</p>
-        <p>{item.biPublishDt}</p>
-      </div>
-    );
-  });
+const Cook = () => {
   return (
     <div className="cook-box">
       <div>
@@ -18,7 +8,6 @@ const Cook = (props) => {
           <h2 className="header-txt">요리</h2>
         </div>
       </div>
-      <div>{list}</div>
     </div>
   );
 };
